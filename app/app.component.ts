b1a31dbbad75179f29bfd4dfe8f86af591e1b878
jarosciak@gmail.com <Gith1974>
2016-09-15 22:10:47 -0400

    initial changes

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,51 +6,63 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  results:any = [];
-  num1:number;
-  divider:number;
-  length:number;
-  input:string;
-  numbers:number;
-
+//  results:any = [];
+  MortgageAmount:number;
+  MortgageAmortizationInMonths:number;
+  InterestRate:number;
+  MonthlyInterestRate:number;
+  MortgageAmortizationConvertedtoYears:number;
+  MortgageAmortizationConvertedtoMonths:number;
+  MortgagePayment:number;
+  PaymentFrequency:string;
 
   ngOnInit() {
     console.log("--APP Started--");
-    this.num1 = 37540;
-    this.divider = 3;
+    this.MortgageAmount = 100000;
+    this.MortgageAmortizationInMonths = 240;
+    this.InterestRate = 6.6;
+    this.PaymentFrequency = "Monthly";
   }
 
   pushMe() {
-    var startCharPosition:any;
-    var endCharPosition:any;
-    var length:number;
-    var tempNumber:any;
-    var divider:number;
-    var input:string;
-
-    //this.results.clear;
-    input = this.num1.valueOf().toString();
-    divider = this.divider;
-
-    length = input.length + 1;
-    console.log("Length: " + length);
-    console.log("Divider: " + divider);
-    console.log("Input: " + input);
-
     console.log("--START--");
+    //this.results = [];
+    //var input:any;
 
-    for (startCharPosition = 0; startCharPosition < length; startCharPosition++) {
-      for (endCharPosition = startCharPosition; endCharPosition < length; endCharPosition++) {
-        if (startCharPosition != endCharPosition) {
-          tempNumber = input.substr(startCharPosition, endCharPosition);
-          if (parseInt(tempNumber) % divider === 0) {
-            this.results[startCharPosition] = tempNumber;
-            console.log(tempNumber);
-          }
-        }
+    /*
+     Converting a Semi-Annual Rate to a Monthly Rate
+     If the nominal interest rate is R (expressed as a decimal rather than percent), then the monthly interest rate is
+     (1 + R/2)1/6 - 1.
+     For example, if the annual interest rate is R = 0.066 (6.6%), then the monthly rate is
+     (1 + 0.033)1/6 - 1 = 0.00542587 (equivalently 0.542587%).
+     */
 
-      }
+    this.MortgageAmortizationConvertedtoYears = Math.floor(this.MortgageAmortizationInMonths / 12);
+    this.MortgageAmortizationConvertedtoMonths = this.MortgageAmortizationInMonths % 12;
+    this.MonthlyInterestRate = +(Math.pow(1 + ((this.InterestRate/100)/2) , 1/6)-1).toFixed(8);
+    console.log("MonthlyInterestRate: " + this.MonthlyInterestRate);
+
+    /*
+ Calculating Monthly Payments and Total Interest
+ If the monthly interest rate is i (as a decimal), the number of years N, and the principal P, then the monthly payment is given by the equation
+ Monthly Payment = Pi(1+i)12N/[(1+i)12N - 1].
+ For example, suppose you borrow $100,000 for 20 years at a nominal annual rate of 6.6%. Your three variables are P = 100000, N = 20, and i = 0.00542587. Your monthly payments are
+ $100000(0.00542587)(1.00542587)240/[(1.00542587)240 - 1]
+ = (542.587)(3.664488)/(2.664488)
+ = $746.22.
+ The total interest paid is the difference between the sum of all the monthly payments and the amount borrowed. For example, if you pay $746.22 a month for 20 years and the amount borrowed is $100,000, then the total interest is
+ $746.22(240) - $100,000 = $79,092.80.
+ */
+
+    this.MortgagePayment = +(((this.MortgageAmount*this.MonthlyInterestRate)*(Math.pow(1+this.MonthlyInterestRate, this.MortgageAmortizationInMonths))
+     / (Math.pow(1+this.MonthlyInterestRate, this.MortgageAmortizationInMonths)-1)).toFixed(2));
+
+    if (this.PaymentFrequency==="Bi-Weekly") {
+      this.MortgagePayment = +(((this.MortgagePayment*12)/26).toFixed(2));
     }
+
+    //
+    console.log("MortgagePayment: " + this.MortgagePayment);
     console.log("--END--");
   }
 }
