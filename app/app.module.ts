4b4b6ad846ac378f6ed1142acf0f06550bf37047
Fábio Antunes <fabioantuness@gmail.com>
2016-10-07 11:23:02 +0100

    Query Params

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,13 +2,23 @@ import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppComponent}  from './app.component';
+import {RootComponent}  from './root.component';
 import {ChartModule} from 'primeng/primeng';
 import {TooltipModule} from 'primeng/primeng';
+import { routing, mainRoutingProviders } from './main.routes';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, ReactiveFormsModule,ChartModule,TooltipModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    ChartModule,
+    FormsModule,
+    mainRoutingProviders,
+    ReactiveFormsModule,
+    routing,
+    TooltipModule
+  ],
+  declarations: [AppComponent, RootComponent],
+  bootstrap: [RootComponent]
 })
 export class AppModule {
 }
